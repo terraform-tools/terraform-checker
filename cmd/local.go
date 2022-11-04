d0320cfdd5b9feb96dcff2832ee797b779cec0df
Vincent Chenal <vincent.chenal@protonmail.com>
2022-11-04 16:54:19 +0100

    feat: allow executing only certain check in local mode

--- cmd/local.go
+++ cmd/local.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/terraform-tools/terraform-checker/pkg/filter"
 	"github.com/terraform-tools/terraform-checker/pkg/local"
+	"github.com/terraform-tools/terraform-checker/pkg/terraform"
+)
+
+var (
+	fmtCheck      bool //nolint:gochecknoglobals // don't think there's another way
+	validateCheck bool //nolint:gochecknoglobals // don't think there's another way
+	tfLintCheck   bool //nolint:gochecknoglobals // don't think there's another way
 )
 
 func LocalCmd() *cobra.Command {
-	return &cobra.Command{
+	localCmd := &cobra.Command{
 		Use:   "local",
 		Short: "run terraform-checker in local mode",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
@@ -18,7 +26,26 @@ func LocalCmd() *cobra.Command {
 			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			local.StartLocal(args[0], parallelism)
+			tfChecksTypes := []string{}
+			if fmtCheck {
+				tfChecksTypes = append(tfChecksTypes, terraform.Fmt.String())
+			}
+			if validateCheck {
+				tfChecksTypes = append(tfChecksTypes, terraform.Validate.String())
+			}
+			if tfLintCheck {
+				tfChecksTypes = append(tfChecksTypes, terraform.TFLint.String())
+			}
+
+			if len(tfChecksTypes) == 0 {
+				tfChecksTypes = terraform.AllTfCheckTypes()
+			}
+
+			local.StartLocal(args[0], parallelism, filter.TfCheckTypeFilter{TfCheckTypes: tfChecksTypes})
 		},
 	}
+	localCmd.PersistentFlags().BoolVarP(&fmtCheck, "fmt", "", false, "Whether to execute fmt check or not")
+	localCmd.PersistentFlags().BoolVarP(&validateCheck, "validate", "", false, "Whether to execute validate check or not")
+	localCmd.PersistentFlags().BoolVarP(&tfLintCheck, "tflint", "", false, "Whether to execute tflint check or not")
+	return localCmd
 }
--- pkg/local/local.go
+++ pkg/local/local.go
@@ -7,12 +7,13 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/rs/zerolog/log"
+	"github.com/terraform-tools/terraform-checker/pkg/filter"
 	"github.com/terraform-tools/terraform-checker/pkg/terraform"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
-func StartLocal(dir string, parallelism uint) {
+func StartLocal(dir string, parallelism uint, checkTypes filter.TfCheckTypeFilter) {
 	// SYNCHRONIZATION
 	// Wait group for waiting all tasks to be done in the end
 	var tasksDone sync.WaitGroup
@@ -36,7 +37,7 @@ func StartLocal(dir string, parallelism uint) {
 			defer tasksDone.Done()
 
 			relDir := strings.TrimPrefix(strings.ReplaceAll(tfDir, dir, ""), "/")
-			for _, check := range terraform.GetTfChecks(tfDir, relDir, terraform.AllTfCheckTypes()) {
+			for _, check := range terraform.GetTfChecks(tfDir, relDir, checkTypes.TfCheckTypes) {
 				check.Run()
 				if _, ok := checks[check.Dir()]; !ok {
 					checks[check.Dir()] = []terraform.TfCheck{}
